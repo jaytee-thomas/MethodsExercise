e92aedae47692db2276ed62c6b6511da934b8147
Jaytee-Thomas <jt.aws.cloud@protonmail.com>
2023-11-03 14:36:22 -0500

    Assignment complete

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -2,7 +2,7 @@
 {
     public class Program
     {
-        static void aboutYou()
+        static void AboutYou()
         {
             Console.WriteLine("Please Eneter Your Name: ");
             Console.ReadLine();
@@ -14,30 +14,38 @@
             Console.ReadLine();
         }
 
-        static void addition(int num1, int num2)
+        static int Addition(int num1, int num2)
         {
             int result = num1 + num2;
             Console.WriteLine("Result: " + result);
+            return result;
         }
 
-        static void multiply(int num1, int num2)
+        static int Multiply(int num1, int num2)
         {
             int result = num1 * num2;
             Console.WriteLine("Result: " + result);
+            return result;
         }
 
-        static void divide(int num1, int num2)
+        static double Divide(double num1, double num2)
         {   
-            int result = num1 / num2;
+            double result = num1 / num2;
             Console.WriteLine("Result: " + result);
+            return result;
         }
-
+        static int Mod(int num1, int num2)
+        {
+            int result = num1 % num2;
+            Console.WriteLine("Result: " + result);
+            return result;
+        }
 
 
         static void Main(string[] args)
         {
             
-            aboutYou();
+            AboutYou();
 
             Console.WriteLine("Enter num1: ");
             var num1 = Convert.ToInt32(Console.ReadLine());
@@ -45,13 +53,36 @@
             Console.WriteLine("Enter num2: ");
             var num2 = Convert.ToInt32(Console.ReadLine());
 
-            addition(num1, num2);
+            int sum = Addition(num1, num2);
 
-            multiply(num1, num2);
+            int product = Multiply(num1, num2);
 
-            divide(num1, num2);
+            double quotient = Divide(num1, num2);
 
-            Console.WriteLine("This is a summary of your entries: ");
+            int remainder = Mod(num1, num2);
+
+            /*Console.WriteLine("Summary of your entries:");
+            Console.WriteLine($"Num1: {num1}");
+            Console.WriteLine($"Num2: {num2}");
+            Console.WriteLine($"Sum: {sum}");
+            Console.WriteLine($"Product: {product}");
+            Console.WriteLine($"Quotient: {quotient}");
+            Console.WriteLine($"Remainder: {remainder}");*/
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine( "   Summary of your entries:");
+            Console.WriteLine();
+            Console.WriteLine( " | Operation |    Result    ");
+            Console.WriteLine( " |---------- |--------------");
+            Console.WriteLine($" | Addition  | {sum}        ");
+            Console.WriteLine($" | Multily   | {product}    ");
+            Console.WriteLine($" | Division  | {quotient}   ");
+            Console.WriteLine($" | Modulus   | {remainder}  ");
+            Console.WriteLine( " |___________|______________");
 
             Console.ReadKey();
 
